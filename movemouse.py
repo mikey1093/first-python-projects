9e64d241f08a2252d47c0c59541767b90449a345
mikey1093 <72893441+mikey1093@users.noreply.github.com>
2020-12-04 15:06:32 -0500

    Update movemouse.py

--- movemouse.py
+++ movemouse.py
@@ -4,7 +4,6 @@
 ## Mouse mover to prevent your PC from falling asleep ##
 ########################################################
 
-
 import mouse 
 from pynput.mouse import Button, Controller
 from time import *
@@ -13,37 +12,37 @@ import threading
 import pyautogui
 
 
-
 root = Tk()
-root.title('Move Mouse')
-root.geometry('200x50')
-
+root.title("CrackMouse")
+root.geometry("300x50")
 
 def refresh():
     root.update()
     root.after(1000,refresh)
 
 def start_move():
+    pyautogui.FAILSAFE = False
     refresh()
-    threading.Thread(target=move).start()
+    t = threading.Thread(target=move).start()
+    t.setDaemon(True)
+    t.start()
 
 def move():
     while var.get() == 1:
-        pyautogui.move(0,1)
-        sleep(3)
-        pyautogui.move(0, -1)
-        sleep(3)
+        pyautogui.move(0, 5)
+        sleep(2)
+        pyautogui.press("shift")
+        pyautogui.move(0, -5)
+        sleep(2)
         var.get()
         if var.get() == 0:
             break
 
-
 var = IntVar()
 
-c = Checkbutton(root, text='Move Mouse', variable=var, command=start_move)
+c = Checkbutton(root, text="Move Mouse", variable=var, command=start_move)
 c.pack()
 myLabel = Label(root, text=var.get()).pack
-
 
 root.mainloop()
 
